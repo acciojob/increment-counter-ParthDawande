c910519bdca2f47d7075abdeeca435f85baf1864
ParthDawande <125895972+ParthDawande@users.noreply.github.com>
2025-11-14 15:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let counterElement = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click",()=>{
+	let curr = parseInt(counterElement.textContent);
+	aler(curr);
+	counterElement.textContent = curr+1;
+})
